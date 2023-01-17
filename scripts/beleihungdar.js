d1fc8ff00263d06afd4ddaa7061c760934c0d882
l1v9l9s7l <l1v9l9s7l@gmail.com>
2023-01-17 21:36:20 +0300

    add belihung logic

--- scripts/beleihungdar.js
+++ scripts/beleihungdar.js
@@ -1,11 +1,17 @@
 const button = document.getElementById("submitButton");
 const form = document.getElementById("form");
 const clientId = document.getElementById("client-id");
+
 const darInput = document.getElementById("darInput");
 const oneInput = document.getElementById("darInput");
 const twoInput = document.getElementById("darInput");
 const threeInput = document.getElementById("darInput");
 const fourInput = document.getElementById("darInput");
+
+const priceOne = document.getElementById("priceOne");
+const priceTwo = document.getElementById("priceTwo");
+const amount = document.getElementById("amount");
+const steur = document.getElementById("steur");
 
 const checkRes = (res) => {
   if (res.ok) {
@@ -13,6 +19,19 @@ const checkRes = (res) => {
   }
   return Promise.reject(`Ошибка: ${res.status}`);
 };
+
+$("input").on("input invalid", function () {
+  this.setCustomValidity("");
+  if (this.validity.valueMissing) {
+    this.setCustomValidity("Geben Sie einen Wert ein");
+  }
+  if (this.validity.typeMismatch) {
+    this.setCustomValidity("Geben Sie einen Bruchwert ein");
+  }
+  if (this.validity.patternMismatch) {
+    this.setCustomValidity("Geben Sie einen Bruchwert ein");
+  }
+});
 
 const request = () => {
   return fetch(`loan/calculate/${clientId.value}`, {
@@ -23,7 +42,14 @@ const request = () => {
     body: JSON.stringify({
       darInput: darInput.value,
     }),
-  }).then(checkRes);
+  })
+    .then(checkRes)
+    .then((res) => {
+      priceOne.textContent(`${res.fee} €`);
+      priceTwo.textContent(`${res.interestFee} €`);
+      amount.textContent(`${res.processingFee} €`);
+      steur.textContent(`${res.storageFee} €`);
+    });
 };
 
 function handler() {
@@ -34,5 +60,3 @@ function handler() {
 darInput.oninput = function () {
   handler();
 };
-
-button.addEventListener("click", handler);
